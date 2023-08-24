38b0845fd1d2af24a210cc80dd84f63bf7e83380
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-08-24 23:49:43 +0200

    add more info modale (end bonus)

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,34 @@ const { createApp } =  Vue;
 createApp({
   data(){
     return {
-      songsArray: []
+      songsArray: [],
+      singleSongInfo : {},
+      modalOpen : false
+    }
+  },
+  methods: {
+    moreInfo(index) {
+      axios
+      .get(`http://localhost/php/004-php-dischi-json/api.php?id_song=${index}`)
+      .then((res)=> {
+        this.singleSongInfo = res.data;
+        this.modalOpen = true
+       
+        console.log(res.data)
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+    },
+    closeModal(){
+      this.modalOpen = false;
     }
   },
   created(){
     axios
       .get('http://localhost/php/004-php-dischi-json/api.php')
       .then((res)=> {
-
         this.songsArray = res.data;
-        console.log(res.data);
       })
       .catch((error)=>{
         console.log(error);
